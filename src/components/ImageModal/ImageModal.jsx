be2ea1b23cde6ea83fd2e9c0b443f9227cfc9edc
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-05-23 13:50:21 +0900

    update ImageModal.jsx

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -2,7 +2,17 @@ import ReactModal from "react-modal";
 import css from "./ImageModal.module.css";
 const ImageModal = ({ isOpen, image, onClose }) => {
   return (
-    <ReactModal isOpen={isOpen} onRequestClose={onClose}>
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      style={{
+        content: {
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        },
+      }}
+    >
       <img className={css.img} src={image.urls.regular} />
     </ReactModal>
   );
